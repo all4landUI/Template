6dfff95d3071718df0ef412ef51cb755aadcdfad
Oh <okm7327@naver.com>
2019-04-04 17:32:31 +0900

    업데이트2

---
+++ html/2018/login.php
@@ -0,0 +1,81 @@
+<?php
+    include_once('../../head.php');
+?>
+
+	<!-- Container -->
+	<article class="container">
+    
+        <!-- Sub Menu -->
+        <section id="lnb" class="lnb">
+            <h2>로그인</h2>
+            <ul role="menu">
+                <li class=""><a href="">회원가입</a></li>
+                <li class="current"><a href="#">로그인</a></li>
+                <li class=""><a href="#">아이디/비밀번호 찾기</a></li>
+            </ul>
+        </section>
+        <!-- /Sub Menu -->
+            
+        <section id="content" class="content">
+            <h1 class="title-sub-h1">로그인</h1>
+            
+            <!-- management wrap -->
+            <nav class="tab-menu type-menu ea2">
+                <button type="button" rel="tabLogin1" class="current">행정전자서명 로그인</button>
+                <button type="button" rel="tabLogin2">일반 로그인</button>
+            </nav>
+            
+            <div class="tab-content">
+                <!-- 행정전자서명 로그인 -->
+                <div id="tabLogin1" style="display: block;">
+                    <div class="i-user-form">
+                        <form action="" id="" name="">
+                            <fieldset>
+                            <legend>행정전자서명 로그인</legend>
+                            <div>
+                                <h3><img src="../../../images/ico/ico_login1.png" alt="아이콘"><em>행정전자서명</em>로그인</h3>
+                                <p>행정전자서명 로그인이 안 될 경우,아이디/비밀번호 로그인 후<br>
+                                    행정전자서명 관리에서 행정전자서명 초기화 및 등록을 진행해주십시오.</p>
+                                <button type="button" title="새창열림" class="bt login full bt-2">행정전자서명 로그인</button>
+                                <a href="join.html"><img src="../../../images/ico/ico_login2.png" alt="아이콘">회원가입  &gt;</a>
+                                <a href=""><img src="../../../images/ico/ico_login3.png" alt="아이콘">행정전자서명 다운로드  &gt;</a>
+                            </div>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+                <!-- /행정전자서명 로그인 -->
+                
+                <!-- 일반 로그인 -->
+                <div id="tabLogin2">
+                    <div class="i-user-form">
+                        <form action="" id="" name="">
+                            <fieldset>
+                            <legend>일반 로그인</legend>
+                            <div>
+                                <h3><img src="../../../images/ico/ico_login1.png" alt="아이콘"><em>일반</em>로그인</h3>
+                                <dl>
+                                    <dt><label for="u-id"><span class="blind">아이디</span></label></dt>
+                                    <dd><input type="text" id="u-id" name="u-id" value="" placeholder="아이디"></dd>
+                                    <dt><label for="u-pw"><span class="blind">비밀번호</span></label></dt>
+                                    <dd><input type="password" id="u-pw" name="u-pw" value="" placeholder="비밀번호"></dd>
+                                </dl>
+                                <button type="button" title="새창열림" class="bt login full bt-2">로그인</button>
+                                <a href="join.html"><img src="../../../images/ico/ico_login2.png" alt="아이콘">아이디 찾기  &gt;</a>
+                                <a href=""><img src="../../../images/ico/ico_login3.png" alt="아이콘">비밀번호 찾기  &gt;</a>
+                            </div>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            <!-- management wrap -->
+
+        </section>
+        <!-- /Contents -->
+    
+    </article>
+	<!-- /Container -->
+
+<?php
+    include_once('../../foot.php');
+?>
---
+++ html/2018/table.php
@@ -0,0 +1,271 @@
+<?php
+    include_once('../../head.php');
+?>
+	
+
+	<!-- Container -->
+	<article class="container">
+    
+        <!-- Sub Menu -->
+        <section id="lnb" class="lnb">
+            <h2>서비스관리</h2>
+            <ul role="menu">
+                <li class=""><a href="">소개</a></li>
+                <li class=""><a href="#">서비스 정보</a></li>
+                <li class="current"><a href="#">표 정보</a></li>
+                <li class=""><a href="#">행사 및 일정</a></li>
+            </ul>
+        </section>
+        <!-- /Sub Menu -->
+            
+        <section id="content" class="content">
+            <h1 class="title-sub-h1">표 정보</h1>
+            
+            <!-- Table wrap -->
+            <div class="table-horizontal">
+                <table>
+                    <colgroup>
+                        <col style="width: 110px;">
+                        <col>
+                        <col style="width: 80px;">
+                        <col style="width: 80px;">
+                        <col style="width: 190px;">
+                        <col style="width: 190px;">
+                    </colgroup>
+                    <caption class="blind">표 정보</caption>
+                    <thead>
+                        <tr>
+                            <th scope="col" rowspan="2">중점<br>추진과제</th>
+                            <th scope="col" rowspan="2">세부 추진과제</th>
+                            <th scope="col" rowspan="2">목표<br>(착수)</th>
+                            <th scope="col" rowspan="2">예산<br>(억원)</th>
+                            <th scope="col" colspan="2">추진주체</th>
+                        </tr>
+                        <tr>
+                            <th scope="col">주관</th>
+                            <th scope="col">협조</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">합계</th>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>1,071</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" rowspan="9">1) 육상기인<br>오염원관리</th>
+                            <td>1-1. 하수관로 및 환경기초시설 확충</td>
+                            <td class="text-center">&nbsp;</td>
+                            <td class="text-center">857.4</td>
+                            <td>강진군·완도군·해남군·<br>장흥군(상하수도사업소)</td>
+                            <td>환경부(생활하수과)</td>
+                        </tr>
+                        <tr>
+                            <td>(1-1-1) 강진군 공공하수시설 설치</td>
+                            <td class="text-center">(2014)</td>
+                            <td class="text-center">266</td>
+                            <td>강진군(상하수도사업소)</td>
+                            <td>〃</td>
+                        </tr>
+                        <tr>
+                            <td>(1-1-2) 완도군 소규모 공공하수시설 설치</td>
+                            <td class="text-center">(2014)</td>
+                            <td class="text-center">530</td>
+                            <td>완도군(상하수도사업소)</td>
+                            <td>〃</td>
+                        </tr>
+                        <tr>
+                            <td>(1-1-3) 해남군 소규모 공공하수시설 설치</td>
+                            <td class="text-center">2015</td>
+                            <td class="text-center">14.4</td>
+                            <td>해남군(상하수도사업소)</td>
+                            <td>〃</td>
+                        </tr>
+                        <tr>
+                            <td>(1-1-4) 장흥군 공공하수시설 설치</td>
+                            <td class="text-center">2015</td>
+                            <td class="text-center">47</td>
+                            <td>장흥군(상하수도사업소)</td>
+                            <td>〃</td>
+                        </tr>
+                        <tr>
+                            <td>1-2. 해양환경을 고려한 방조제 관리</td>
+                            <td class="text-center">&nbsp;</td>
+                            <td class="text-center">93.5</td>
+                            <td>농림축산식품부<br>(농업기반과),<br>강진군<br>(해양산림과, 안전건설과),<br>완도군(해양수산과)</td>
+                            <td>해양수산부<br>(해양환경정책과,<br>양식산업과),<br>국립수산과학원(어장환경과)</td>
+                        </tr>
+                        <tr>
+                            <td>(1-2-1) 사내방조제 담수호 최적 관리방안 마련</td>
+                            <td class="text-center">(2016)</td>
+                            <td class="text-center">5</td>
+                            <td>강진군(해양산림과,<br>안전건설과),<br>완도군(해양수산과)</td>
+                            <td>농림축산식품부<br>(농업기반과),<br>해양수산부(해양환경정책과,<br>양식산업과),<br>국립수산과학원(어장환경과)</td>
+                        </tr>
+                        <tr>
+                            <td>(1-2-2) 노후 방조제 시설 개․보수</td>
+                            <td class="text-center">(2014)</td>
+                            <td class="text-center">88.5</td>
+                            <td>농림축산식품부(농업기반과)</td>
+                            <td>강진군(안전건설과)</td>
+                        </tr>
+                        <tr>
+                            <td>1-3. 육상양식장 배출수 수질관리 강화 및 정기점검</td>
+                            <td class="text-center">(2016)</td>
+                            <td class="text-center">0.8</td>
+                            <td>전라남도(환경관리과)</td>
+                            <td>완도군(환경녹지과)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" rowspan="7">2) 해양환경 개선</th>
+                            <td>2-1. 해양환경 오염원인 규명 및 해양환경 개선</td>
+                            <td class="text-center">&nbsp;</td>
+                            <td class="text-center">8</td>
+                            <td>해양수산부(해양환경정책과)</td>
+                            <td>강진군(해양산림과)<br>완도군(해양수산과)</td>
+                        </tr>
+                        <tr>
+                            <td>(2-1-1) 도암만 하계 수질 정밀지단 및 개선계획<br><span></span>수립</td>
+                            <td class="text-center">(2016)</td>
+                            <td class="text-center">5</td>
+                            <td>해양수산부(해양환경정책과)</td>
+                            <td>강진군(해양산림과)</td>
+                        </tr>
+                        <tr>
+                            <td>(2-1-2) 퇴적물 오염원조사 및 저감방안 마련</td>
+                            <td class="text-center">(2016)</td>
+                            <td class="text-center">3</td>
+                            <td>해양수산부(해양환경정책과)</td>
+                            <td>강진군(해양산림과),<br>완도군(해양수산과)</td>
+                        </tr>
+                        <tr>
+                            <td>2-2. 퇴적물 환경개선을 위한 정화․복원</td>
+                            <td class="text-center">&nbsp;</td>
+                            <td class="text-center">15</td>
+                            <td>해양수산부(해양보전과,<br>양식산업과)</td>
+                            <td>강진군(해양산림과),<br>완도군, 해남군,<br>장흥군 (해양수산과)</td>
+                        </tr>
+                        <tr>
+                            <td>(2-2-1) 오염 우심해역 퇴적물 복원사업</td>
+                            <td class="text-center">(2018)</td>
+                            <td class="text-center">5</td>
+                            <td>해양수산부(해양보전과)</td>
+                            <td>완도군(해양수산과)</td>
+                        </tr>
+                        <tr>
+                            <td>(2-2-2) 어장 퇴적물 정화사업</td>
+                            <td class="text-center">(2014)</td>
+                            <td class="text-center">10</td>
+                            <td>해양수산부(양식산업과)</td>
+                            <td>강진군(해양산림과),<br>완도군, 해남군,<br>장흥군 (해양수산과)</td>
+                        </tr>
+                        <tr>
+                            <td>2-3. 해양쓰레기 수거․처리</td>
+                            <td class="text-center">(2014)</td>
+                            <td class="text-center">20.1</td>
+                            <td>해양수산부(해양보전과)</td>
+                            <td>강진군(해양산림과),<br>해남군,<br>장흥군(해양수산과)</td>
+                        </tr>
+                        <tr>
+                            <th rowspan="5">3) 해양 생태계<br>보호 및 <br>서식지 관리</th>
+                            <td>3-1. 강진군 연안 바다목장 사업</td>
+                            <td class="text-center">2015</td>
+                            <td class="text-center">50</td>
+                            <td>강진군(해양산림과)</td>
+                            <td>수산자원관리공단<br>(남해지사 자원조성실)</td>
+                        </tr>
+                        <tr>
+                            <td>3-2. 생태계 기반 어장 관리</td>
+                            <td class="text-center">(2014)</td>
+                            <td class="text-center">3</td>
+                            <td>완도군(해양수산과),<br>해양수산부(양식산업과)</td>
+                            <td>강진군(해양산림과),<br>완도군, 해남군,<br>장흥군 (해양수산과)</td>
+                        </tr>
+                        <tr>
+                            <td>3-3. 환경 친화적 양식어구 및 부산물 관리</td>
+                            <td class="text-center">&nbsp;</td>
+                            <td class="text-center">10</td>
+                            <td>완도군(해양수산과),<br>해양수산부(양식산업과)</td>
+                            <td>완도군(해양수산과)</td>
+                        </tr>
+                        <tr>
+                            <td>(3-3-1) 양식 해조류 부산물 재활용</td>
+                            <td class="text-center">(2014)</td>
+                            <td class="text-center">5</td>
+                            <td>완도군(해양수산과)</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>(3-3-2) 개량부자 공급</td>
+                            <td class="text-center">(2014)</td>
+                            <td class="text-center">5</td>
+                            <td>해양수산부(양식산업과)</td>
+                            <td>완도군(해양수산과</td>
+                        </tr>
+                        <tr>
+                            <th rowspan="7">4) 해양환경<br>관리 역량 강화</th>
+                            <td>4-1. 완도․도암만 환경보전해역 관리기반 구축</td>
+                            <td class="text-center">(2015)</td>
+                            <td class="text-center">3</td>
+                            <td>해양수산부(해양환경정책과)</td>
+                            <td>목포지방해양항만청<br>(해양환경과),<br>국립수산과학원<br>(남서해수산연구소),<br>전라남도(해양항만과),<br>강진군(해양산림과), 완도군,<br>해남군, 장흥군(해양수산과)</td>
+                        </tr>
+                        <tr>
+                            <td>4-2. 완도․도암만 과학적 진단체계 개선</td>
+                            <td class="text-center">&nbsp;</td>
+                            <td class="text-center">4.3</td>
+                            <td>해양수산부(해양환경정책과,<br>해양생태과),<br>농림축산식품부(농업기반과)</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>(4-2-1) 연안습지 및 해양생태계 통합조사</td>
+                            <td class="text-center">(2016)</td>
+                            <td class="text-center">2</td>
+                            <td>해양수산부(해양생태과)</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>(4-2-2) 보호종 서식실태 시범조사</td>
+                            <td class="text-center">(2016)</td>
+                            <td class="text-center">2</td>
+                            <td>해양수산부(해양생태과)</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>(4-2-3) 해양환경 모니터체제 강화</td>
+                            <td class="text-center">2014</td>
+                            <td class="text-center">-</td>
+                            <td>해양수산부(해양환경정책과)</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>(4-2-4) 담수호 수질 모니터링 실시</td>
+                            <td class="text-center">(2016)</td>
+                            <td class="text-center">0.3</td>
+                            <td>농림축산식품부(농업기반과)</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>4-3. 환경관리해역 관리역량 강화 및 홍보</td>
+                            <td class="text-center">(2016)</td>
+                            <td class="text-center">6</td>
+                            <td>해양수산부(해양환경정책과)</td>
+                            <td>목포지방해양항만청<br>(해양환경과)<br>강진군(해양산림과),<br>완도군, 해남군<br>장흥군 (해양수산과)</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- Table wrap -->
+
+        </section>
+        <!-- /Contents -->
+    
+    </article>
+	<!-- /Container -->
+
+<?php
+    include_once('../../foot.php');
+?>
