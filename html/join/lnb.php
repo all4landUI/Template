041efd7ae91ba17eaa3534fdbdf0da772f316bbb
kangsanghwan <47845232+kangsanghwan@users.noreply.github.com>
2019-04-04 18:39:14 +0900

    190404(4)

--- html/join/lnb.php
+++ html/join/lnb.php
@@ -1,6 +1,6 @@
 <h2>로그인</h2>
 <ul role="menu">
-	<li><a href="sitemap.php">회원가입</a></li>
+	<li><a href="join.php">회원가입</a></li>
 	<li><a href="login.php">로그인</a></li>
 	<li><a href="sitemap.php">아이디/비밀번호 찾기</a></li>
 </ul>
