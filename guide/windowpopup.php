8638c3a7bcbca279e2afd76abc138da35072a23c
Oh <okm7327@naver.com>
2019-04-08 10:01:38 +0900

    업데이트!!

--- guide/windowpopup.php
+++ guide/windowpopup.php
@@ -6,18 +6,18 @@
 <link rel="icon" type="image/x-icon" href="images/ico/favicon.ico">
 <link rel="apple-touch-icon-precomposed" type="image/x-icon" href="images/ico/favicon.ico">
 <link rel="shortcut icon" type="image/x-icon" href="images/ico/favicon.ico">
-<link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="/css/fonts/NotoSansKR.css">
-<link rel="stylesheet" type="text/css" href="/js/libs/jQuery-ui/jquery-ui.css">
-<link rel="stylesheet" type="text/css" href="/css/ngii-mng.css">
+<link rel="stylesheet" type="text/css" href="/dist/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="/dist/css/fonts/NotoSansKR.css">
+<link rel="stylesheet" type="text/css" href="/dist/js/libs/jQuery-ui/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="/dist/css/ngii-mng.css">
 <title>UI ELEMENT - 가이드</title>
-<script type="text/javascript" src="/js/libs/jQuery/1.12.3/jquery.min.js"></script>
-<script type="text/javascript" src="/js/libs/jQuery-ui/jquery-ui.min.js"></script>
-<script type="text/javascript" src="/js/ngii-mng.js"></script>
-<script type="text/javascript" src="/js/pub.js"></script>
+<script type="text/javascript" src="/dist/js/libs/jQuery/1.12.3/jquery.min.js"></script>
+<script type="text/javascript" src="/dist/js/libs/jQuery-ui/jquery-ui.min.js"></script>
+<script type="text/javascript" src="/dist/js/ngii-mng.js"></script>
+<script type="text/javascript" src="/dist/js/pub.js"></script>
 <!--[if lt IE 9]>
-	<script type="text/javascript" src="/js/libs/html5shiv.min.js"></script>
-	<script type="text/javascript" src="/js/libs/respond.min.js"></script>
+	<script type="text/javascript" src="/dist/js/libs/html5shiv.min.js"></script>
+	<script type="text/javascript" src="/dist/js/libs/respond.min.js"></script>
 <![endif]-->
 <body style="background:#7cb6ce">
 <!-- #window_popup //-->
